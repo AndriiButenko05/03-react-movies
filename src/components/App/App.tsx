509818517cb6278eff3e210bd90ba08fcf04c3cb
Andrii Butenko <andrejbutik@gmail.com>
2025-08-02 11:03:41 +0200

    Deleted isModal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -14,7 +14,6 @@ export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [movie, setMovie] = useState<Movie | null>(null);
   const [isError, setIsError] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   async function handleSearch(title: string) {
     try {
       setIsError(false);
@@ -34,11 +33,9 @@ export default function App() {
     }
   }
   function openModal(movie: Movie) {
-    setIsModalOpen(true);
     setMovie(movie);
   }
   function closeModal() {
-    setIsModalOpen(false);
     setMovie(null)
   }
   return (
